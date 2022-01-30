4de7e1dde3583d09499393b5485c933faaa57ee5
Harshit Kumar <56219245+Harshitk07@users.noreply.github.com>
2022-01-31 00:17:30 +0530

    fixed few things

--- project1/src/app/app.module.ts
+++ project1/src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule  } 
+from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './MyComponent/todos/todos.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+
+
 
 @NgModule({
   declarations: [
@@ -13,6 +19,11 @@ import { TodosComponent } from './MyComponent/todos/todos.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    NgMultiSelectDropDownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
